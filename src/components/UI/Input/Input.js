8e959cc73cb228a48230183cf957ea13b46867b7
abhishek.handore <abhishek.handore@adapty.com>
2022-02-01 08:41:58 +0530

    useimperative and useref handling

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {useRef, useImperativeHandle} from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+/**
+ * * Here we want to focus on the first invalid input element. 
+ * * But this is not possible with useEffect because it will run after component re-rendered.
+ * * We want to focus an Input element on click of submit. So here we are using useRef 
+ */
+
+
+/**
+ * * To enable the second parameter , we need to export our content in React.forwardRef
+ */
+const Input = React.forwardRef((props, ref) => {
+
+    const inputRef = useRef();
+    const activate = () => {
+        inputRef.current.focus();
+    }
+
+    /**
+     * * It allows us to use this component or functionality inside the component imperatively that means not with the regular state or props management not controlling through the state in the parent component, but directly calling or manipulating something in the component programatically.
+     * 
+     * *2 parameters = {
+     *      -- second parameter is an function which should always return an object and contain all the data you'll be using outside. 
+     *      -- First parameter is an ref which we have accected in the component arguments.
+     * }  
+     * 
+     * * With useImperativeHandle and useRef , you can expose functionalities from react component to its parent component to then use your component in parent component through refs and trigger certain functionalities. 
+     * 
+     * * You should avoid this most of the time. But usecases like this, e.g., focus an input, scrolling etc we can use useImperativeHandle.  
+     */
+    useImperativeHandle(ref, () => {
+        return {
+            focus: activate
+        }
+    })
+
     return (
         <div
         className={`${classes.control} ${
@@ -10,6 +44,7 @@ const Input = (props) => {
         >
         <label htmlFor={props.id}>{props.label}</label>
         <input
+            ref={inputRef}
             type={props.type}
             id={props.id}
             name={props.name}
@@ -19,6 +54,6 @@ const Input = (props) => {
         />
         </div>
     )
-}
+})
 
 export default Input;
